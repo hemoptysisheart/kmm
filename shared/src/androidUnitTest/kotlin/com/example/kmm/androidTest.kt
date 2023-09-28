e664eea2e1c479e7fb8f19ef6664bd9945413c61
H2 <hemoptysisheart@gmail.com>
2023-09-29 02:49:54 +0900

    🧪 UT 작동 확인용으로 테스트 추가.

--- shared/src/androidUnitTest/kotlin/com/example/kmm/androidTest.kt
+++ shared/src/androidUnitTest/kotlin/com/example/kmm/androidTest.kt
@@ -2,11 +2,24 @@ package com.example.kmm
 
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import kotlin.test.fail
 
 class AndroidGreetingTest {
-
     @Test
     fun testExample() {
-        assertTrue("Check Android is mentioned", Greeting().greet().contains("Android"))
+        // GIVEN
+        val greeting = Greeting()
+        println("[GIVEN] greeting=$greeting")
+
+        // WHEN
+        val actual = greeting.greet()
+        println("[WHEN] actual=$actual")
+
+        assertTrue("Check Android is mentioned", actual.contains("Android"))
+    }
+
+    @Test
+    fun `test fail`(){
+        fail("test fail.")
     }
 }
--- shared/src/iosTest/kotlin/com/example/kmm/iosTest.kt
+++ shared/src/iosTest/kotlin/com/example/kmm/iosTest.kt
@@ -2,11 +2,23 @@ package com.example.kmm
 
 import kotlin.test.Test
 import kotlin.test.assertTrue
+import kotlin.test.fail
 
 class IosGreetingTest {
-
     @Test
     fun testExample() {
-        assertTrue(Greeting().greet().contains("iOS"), "Check iOS is mentioned")
+        // GIVEN
+        val greeting = Greeting()
+
+        // WHEN
+        val actual = greeting.greet()
+
+        // THEN
+        assertTrue(actual.contains("iOS"), "Check iOS is mentioned")
+    }
+
+    @Test
+    fun `test fail`(){
+        fail("test fail.")
     }
 }
