c9777f2deaf98034c6bafca0d8aecbab23f23905
H2 <hemoptysisheart@gmail.com>
2023-09-30 22:10:12 +0900

    🚚 인터페이스 이름 변경.
    구체적인 자료구조는 클래스 이름에서 표시.

--- shared/src/commonMain/kotlin/com/example/kmm/ds/List.kt
+++ shared/src/commonMain/kotlin/com/example/kmm/ds/List.kt
@@ -1,12 +1,15 @@
 package com.example.kmm.ds
 
-interface SingleLinkedList<E> {
+interface List<E> {
+    /**
+     * 데이터 갯수.
+     */
     val size: Int
 
-    fun add(element: E)
+    fun add(data: E):Int
 
     @Throws(IndexOutOfBoundsException::class)
-    fun add(index: Int, element: E)
+    fun add(index: Int, data: E)
 
     fun first(): E?
 
@@ -15,7 +18,7 @@ interface SingleLinkedList<E> {
     @Throws(IndexOutOfBoundsException::class)
     operator fun get(index: Int): E
 
-    fun contains(element: E): Boolean
+    fun contains(data: E): Boolean
 
     @Throws(IndexOutOfBoundsException::class)
     fun remove(index: Int): E
