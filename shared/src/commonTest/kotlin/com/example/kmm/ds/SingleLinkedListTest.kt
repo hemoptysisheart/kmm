182f974dd2f520c4a9986e31c1927e0f0b6139c3
H2 <hemoptysisheart@gmail.com>
2023-10-25 19:00:45 +0900

    ✨ IDE에 단위테스트 클래스 인식시키기.
    `@Test` 어노테이션이 없어서 테스트 클래스로 인식이 안되는 문제를 해결.

--- shared/src/commonTest/kotlin/com/example/kmm/ds/SingleLinkedListTest.kt
+++ shared/src/commonTest/kotlin/com/example/kmm/ds/SingleLinkedListTest.kt
@@ -1,5 +1,19 @@
 package com.example.kmm.ds
 
+import kotlin.test.Test
+import kotlin.test.assertIs
+
+@Suppress("NonAsciiCharacters")
 class SingleLinkedListTest : AbstractListTest<SingleLinkedList<Long>>() {
     override fun list() = SingleLinkedList<Long>()
+
+    @Test
+    fun `list - 테스트 실행용`() {
+        // WHEN
+        val list = list()
+        println("[WHEN] list=$list")
+
+        // THEN
+        assertIs<SingleLinkedList<Long>>(list)
+    }
 }
