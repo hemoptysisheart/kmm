2220b73b6f0f5fb014c2f402824f2628ccbb5499
H2 <hemoptysisheart@gmail.com>
2023-11-25 10:49:05 +0900

    ✅ 테스트 실행 확인용 케이스 추가.

--- shared/src/commonTest/kotlin/com/example/kmm/ds/DoubleListTest.kt
+++ shared/src/commonTest/kotlin/com/example/kmm/ds/DoubleListTest.kt
@@ -1,5 +1,19 @@
 package com.example.kmm.ds
 
+import kotlin.test.Test
+import kotlin.test.assertIs
+
+@Suppress("NonAsciiCharacters")
 class DoubleListTest : AbstractListTest<DoubleLinkedList<Long>>() {
     override fun list(): DoubleLinkedList<Long> = DoubleLinkedList()
+
+    @Test
+    fun `list - 테스트 실행용`() {
+        // WHEN
+        val list = list()
+        println("[WHEN] list=$list")
+
+        // THEN
+        assertIs<DoubleLinkedList<Long>>(list)
+    }
 }
